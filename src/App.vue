6227ee148691a5b02fbe3276e9563d78b2cc3a8f
Hiroshi-tech <hiro4.4116@gmail.com>
2020-11-29 13:19:45 +0900

    App.vueにHeaderとFooterを追加しました（components)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/signup">Sign up</router-link>
-      <router-link to="/signin">Sign in</router-link>
-      <router-link to="/signout">Sign out</router-link>
-      <router-link to="/default">Default</router-link>
-      <router-link to="/mypage">Mypage</router-link>
-    </div>
+    <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -20,27 +23,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  background-color: #a4a4a4;
-  height: 2.5rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 2rem;
-
-  a {
-    font-weight: bold;
-    color: #fff;
-    text-decoration: none;
-    &:hover {
-      color: #f00;
-    }
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
